da6873a34ae1f573ca18e1a8835826a30f6b2420
Achyuta Das <aka.achu.1612@gmail.com>
2020-08-25 19:31:09 +0530

    refactor: cmd pkg
    1.Documented 2.TLS file path from .env file 3.build details in .env file

--- cmd/execute.go
+++ cmd/execute.go
@@ -5,16 +5,19 @@ import (
 	"github.com/aka-achu/watcher/logging"
 	"net/http"
 	"os"
-	"path/filepath"
 )
 
+// Execute starts the web server on the specified server address
+// in the ".env" file.
 func Execute() {
+	// Initializing the controller, registering the endpoints
 	router := controller.Initialize()
+
 	if os.Getenv("BUILD") == "Prod" {
 		logging.Error.Fatal(http.ListenAndServeTLS(
 			os.Getenv("SERVER_ADDRESS"),
-			filepath.Join("config", "tls.cert"),
-			filepath.Join("config", "tls.key"),
+			os.Getenv("TLS_CERTIFICATE_FILE"),
+			os.Getenv("TLS_KEY_FILE"),
 			router,
 		))
 	} else {
