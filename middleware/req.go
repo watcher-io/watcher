704a452159d9fd6776568b82dee4f2691a892831
Achyuta Das <aka.achu.1612@gmail.com>
2020-08-25 21:42:50 +0530

    doc: middleware pkg documented

--- middleware/req.go
+++ middleware/req.go
@@ -9,20 +9,29 @@ import (
 	"net/http"
 )
 
+// NoAuthLogging is a middleware which will omit the request auth validation
+// and generate a request tracing id.
 func NoAuthLogging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Generating a request tracing id
 		traceID := uuid.New().String()
+		// Storing the generated traceID in the context store against the key "trace_id"
 		ctx := context.WithValue(r.Context(), "trace_id", traceID)
 		logging.Info.Printf(" [Req] ID-%s, URI-%s", traceID, r.RequestURI)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
 
+// AuthLogging is a middleware which will validate the request based on the
+// Authorization token present in the request header and generate a request tracing id.
 func AuthLogging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Generating a request tracing id
 		traceID := uuid.New().String()
+		// Storing the generated traceID in the context store against the key "trace_id"
 		ctx := context.WithValue(r.Context(), "trace_id", traceID)
 		logging.Info.Printf(" [Req] ID-%s, URI-%s", traceID, r.RequestURI)
+		// Validating the Authorization token present in the request header
 		if err := utility.VerifyToken(r.Header.Get("Authorization")); err != nil {
 			logging.Warn.Printf(" [Req] Invalid access token. Error-%b TraceID-%s", err, traceID)
 			response.UnAuthorized(w,"000","Invalid access token")
