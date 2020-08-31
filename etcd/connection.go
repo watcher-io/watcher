466c7fc42a93825a5e3b3039b0980bc71e0b0977
Achyuta Das <aka.achu.1612@gmail.com>
2020-08-31 23:15:56 +0530

    add: connection store integration and ttl test

--- etcd/connection.go
+++ etcd/connection.go
@@ -1,22 +1,23 @@
 package etcd
 
 import (
+	"fmt"
 	"github.com/aka-achu/watcher/model"
 	"go.etcd.io/etcd/clientv3"
 	"time"
 )
 
 func connect(cfg *model.Cluster) (*clientv3.Client, error) {
+	fmt.Println(cfg)
 	if !cfg.TLS {
 		return clientv3.New(clientv3.Config{
 			DialTimeout: 2 * time.Second,
-			Endpoints:            cfg.Endpoints,
-			TLS:                  nil,
-			Username:             cfg.Username,
-			Password:             cfg.Password,
+			Endpoints:   cfg.Endpoints,
+			TLS:         nil,
+			Username:    cfg.Username,
+			Password:    cfg.Password,
 		})
 	} else {
-		return nil,nil
+		return nil, nil
 	}
-
 }
