e48b70ff521f54b2e642786b4795c131acf07b62
Achyuta Das <aka.achu.1612@gmail.com>
2020-09-28 23:04:46 +0530

    add: tls configs added in server

--- cmd/execute.go
+++ cmd/execute.go
@@ -1,12 +1,15 @@
 package cmd
 
 import (
+	"crypto/tls"
 	"github.com/aka-achu/watcher/controller"
 	"github.com/aka-achu/watcher/logging"
+	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 // Execute starts the web server on the specified server address
@@ -29,20 +32,56 @@ func Execute() {
 	} else {
 		router.Use(cors.AllowAll().Handler)
 	}
+	server := getServer(os.Getenv("BUILD") == "Prod", router)
 
 	if os.Getenv("BUILD") == "Prod" {
 		logging.Info.Printf(" [APP] Starting server @%s", os.Getenv("SERVER_ADDRESS"))
-		logging.Error.Fatal(http.ListenAndServeTLS(
-			os.Getenv("SERVER_ADDRESS"),
-			filepath.Join("cert", os.Getenv("TLS_CERTIFICATE_FILE")),
-			filepath.Join("cert", os.Getenv("TLS_KEY_FILE")),
-			router,
-		))
+		logging.Error.Fatal(
+			server.ListenAndServeTLS(
+				filepath.Join("cert", os.Getenv("TLS_CERTIFICATE_FILE")),
+				filepath.Join("cert", os.Getenv("TLS_KEY_FILE")),
+			),
+		)
 	} else {
 		logging.Info.Printf(" [APP] Starting server @%s", os.Getenv("SERVER_ADDRESS"))
-		logging.Error.Fatal(http.ListenAndServe(
-			os.Getenv("SERVER_ADDRESS"),
-			router,
-		))
+		logging.Error.Fatal(
+			server.ListenAndServe(),
+		)
+	}
+}
+
+func getServer(secure bool, router *mux.Router) *http.Server {
+	if secure {
+		return &http.Server{
+			Addr:         os.Getenv("SERVER_ADDRESS"),
+			Handler:      router,
+			ReadTimeout:  5 * time.Second,
+			WriteTimeout: 10 * time.Second,
+			IdleTimeout:  15 * time.Second,
+			TLSConfig: &tls.Config{
+				PreferServerCipherSuites: true,
+				CurvePreferences: []tls.CurveID{
+					tls.CurveP256,
+					tls.X25519, // Go 1.8 only
+				},
+				MinVersion: tls.VersionTLS12,
+				CipherSuites: []uint16{
+					tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
+					tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				},
+			},
+		}
+	} else {
+		return &http.Server{
+			Addr:         os.Getenv("SERVER_ADDRESS"),
+			Handler:      router,
+			ReadTimeout:  5 * time.Second,
+			WriteTimeout: 10 * time.Second,
+			IdleTimeout:  15 * time.Second,
+		}
 	}
 }
